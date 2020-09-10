15f2f1a265da13cbce3293792848a6eb748716ca
Isaiah Wright <icwright902@gmail.com>
2020-09-10 11:39:53 -0600

    token setup

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,14 @@
 require('dotenv').config();
 
 
-// is this working?
+const {
+    Client
+} = require('discord.js');
+
+const client = new Client();
+
+client.on('ready', () => {
+    console.log('the bot has logged in.')
+})
+
+client.login("process.env.DISCORD_BOT_TOKEN");
